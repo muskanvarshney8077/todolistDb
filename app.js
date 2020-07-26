65009cfc18947a4f298cc9fc535433c49ce804e4
muskanvarshney8077 <muskanvarshney8077@gmail.com>
2020-07-26 14:44:18 +0530

    update

--- app.js
+++ app.js
@@ -188,10 +188,8 @@ app.get("/about", function(req, res){
 
 
 
-let port=process.env.PORT;
-if (port ==null || port==" "){
-	port=3000;
-}
+let port=process.env.PORT||3000;
+
 app.listen(port, function() {
   console.log("Server started on port 3000");
 });
